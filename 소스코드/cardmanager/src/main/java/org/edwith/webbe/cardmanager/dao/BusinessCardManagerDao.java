1f215e02b582b696909806a19e4ac1b07edfcc5f
seohuiJo <41426853+seohuiJo@users.noreply.github.com>
2020-08-25 20:26:14 +0900

    mysql workbench 설치

--- 소스코드/cardmanager/src/main/java/org/edwith/webbe/cardmanager/dao/BusinessCardManagerDao.java
+++ 소스코드/cardmanager/src/main/java/org/edwith/webbe/cardmanager/dao/BusinessCardManagerDao.java
@@ -7,10 +7,11 @@ import java.util.List;
 public class BusinessCardManagerDao {
     public List<BusinessCard> searchBusinessCard(String keyword){
 	// 구현하세요.
+        return null;
     }
 
     public BusinessCard addBusinessCard(BusinessCard businessCard){
-
-
+    // mysql workbench 설치
+        return null;
     }
 }
